756eed805ed070b952c8af4f923dc3126f785a86
Alisa Poltoran <alisapoltoran@inbox.ru>
2023-02-20 17:06:25 +0300

    Files added

---
+++ src/main/java/ru/netology/conditionalapp/configuration/JavaConfig.java
@@ -0,0 +1,24 @@
+package ru.netology.conditionalapp.configuration;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.conditionalapp.profile.DevProfile;
+import ru.netology.conditionalapp.profile.ProductionProfile;
+import ru.netology.conditionalapp.profile.SystemProfile;
+
+@Configuration
+public class JavaConfig {
+
+    @Bean
+    @ConditionalOnProperty(prefix = "profile", name = "dev", havingValue = "true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "profile", name = "dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
